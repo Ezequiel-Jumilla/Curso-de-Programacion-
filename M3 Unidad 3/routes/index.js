77470f6c84dabd154f6bac4bcbfe4977dbda79bc
Ezequiel Jumilla <ezequieljumilla328@gmail.com>
2022-04-28 10:16:25 -0300

    Tarea Formulario Unidad 3

--- M3 Unidad 3/routes/index.js
+++ M3 Unidad 3/routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+const res = require('express/lib/response');
+const async = require('hbs/lib/async');
 var router = express.Router();
 var nodemailer = require('nodemailer');
 
@@ -6,5 +8,39 @@ var nodemailer = require('nodemailer');
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+router.post('/', async(req, res, next) =>{
+
+var email = req.body.email;
+var nombre = req.body.nombre;
+var consulta = req.body.consulta;
+
+
+
+var obj = {
+  to: 'ezequieljumilla328@gmail.com',
+  subject: 'CONTACTO WEB KOI',
+  html: nombre + " " + "Se contacto a traves de el formulario de consulta de la web de KOI, esperando mas informacion en el siguiente mail : " + email + ". <br> Además, realizó la siguiente consulta: " + consulta 
+}
+
+var transport =nodemailer.createTransport({
+host: process.env.SMTP_HOST,
+port: process.env.SMTP_PORT,
+auth: {
+  user: process.env.SMTP_USER,
+  pass: process.env.SMTP_PASS
+}
+});
+
+
+var info = await transport.sendMail(obj);
+
+res.render('index', {
+  message: 'Mensaje enviado correctamente'
+});
+});
+
+
 
 module.exports = router;
