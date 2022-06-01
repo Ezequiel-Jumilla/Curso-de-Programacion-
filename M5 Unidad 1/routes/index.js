1d0a607b85a83c35772a34d2b0dae94f15279321
Ezequiel Jumilla <ezequieljumilla328@gmail.com>
2022-06-01 13:23:09 -0300

    Tarea Modulo 5 U1

---
+++ M5 Unidad 1/routes/index.js
@@ -0,0 +1,51 @@
+var express = require('express');
+const res = require('express/lib/response');
+const async = require('hbs/lib/async');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+var novedadesModel = require('../models/novedadesModel');
+
+
+/* GET home page. */
+router.get('/', async function (req, res, next) {
+
+  var novedades = await novedadesModel.getnovedades();
+
+  res.render('index', { novedades });
+});
+
+
+router.post('/', async (req, res, next) => {
+
+  var email = req.body.email;
+  var nombre = req.body.nombre;
+  var consulta = req.body.consulta;
+
+
+
+  var obj = {
+    to: 'ezequieljumilla328@gmail.com',
+    subject: 'CONTACTO WEB KOI',
+    html: nombre + " " + "Se contactó a través de el formulario de consulta de la web de KOI, esperando mas información en el siguiente mail : " + email + ". <br> Además, realizó la siguiente consulta: " + consulta
+  }
+
+  var transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+  });
+
+
+  var info = await transport.sendMail(obj);
+
+  res.render('index', {
+    message: 'Mensaje enviado correctamente'
+  });
+});
+
+
+
+module.exports = router;
